eb1ae1eaba2f54f1b232a93945dc580316d6c5a3
tak357 <52248763+tak357@users.noreply.github.com>
2020-03-25 22:54:40 +0900

    404ページを作成

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', '404 Page Not Found')
+
+@section('content')
+
+    <h2 class="head-gray">404 Page Not Found</h2>
+    <p>ページが見つかりません。</p>
+    <p>無効なURLです。</p>
+    <p><a href="/">トップページへ戻る</a></p>
+
+@endsection
